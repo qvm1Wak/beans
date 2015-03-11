860e3f0b675b71cbef1ea2e0c657a2ef6861deed
Steven <example@example.com>
2015-03-11 09:31:31 +0000

    streak detection, calibration, faster sampling, error handling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var path = require('path');
 var program = require('commander');
 var util = require('util');
 var winston = require('winston');
+var summary = require('summary');
 
 var logger = new (winston.Logger)({
   transports: [
@@ -23,9 +24,6 @@ var logger = new (winston.Logger)({
     })
   ]
 });
-// winston.add(winston.transports.File, { filename: 'debug.log' });
-// winston.remove(winston.transports.Console);
-// var logger = winston;
 
 omx.enableHangingHandler();
 
@@ -40,7 +38,6 @@ program.version('0.0.1')
 logger.log('info', 'Application started' + (program.debug ? ' [debug]' : (program.calibrate ? ' [calibrate]' : '')));
 logger.log('debug', 'Application started' + (program.debug ? ' [debug]' : (program.calibrate ? ' [calibrate]' : '')));
 
-
 // Hack: mokeypatch for fs
 fs.existsSync = require('path').existsSync;
 
@@ -50,70 +47,61 @@ var greenPin = 13;
 var bluePin = 15;
 var photoPin = 12;
 
-var CAPACITOR_POLL_DELAY = 10;
+var DECAY_RATE = 20;
 
 var pinTimer = (function () {
-  /*
-   
-   */
   var isTiming = {};
   return function (pin, cb) {
+    // console.log('timing', pin);
     if (isTiming[pin+'']) return;
     isTiming[pin+''] = true;
     var counter = 0;
     var readPoller = function (pin, cb) {
-      logger.log('debug', '1');
+      if (!isTiming[pin+'']) { cb(-1); return; } // in case of errors elsewhere
       gpio.read(pin, function (err, value) {
-        //logger.log('debug', '2');
-        if (err) { isTiming[pin+''] = false; logger.log('debug', 'error'); }
-        if (value === 1) { isTiming[pin+''] = false; return cb(counter); }
-        if (counter > 150) { isTiming[pin+''] = false; return cb(Infinity); }
+        if (err) { isTiming[pin+''] = false; logger.log('debug', 'error 1'); cb(-1); return; }
+        if (value === 1) { isTiming[pin+''] = false; cb(counter); return; }
+        if (counter > 150) { isTiming[pin+''] = false; cb(Infinity); return; }
+        // console.log('timing 5', pin, counter);
+
         counter++;
-        //logger.log('debug', '3');
-        setTimeout(function () {
-          //logger.log('debug', '4');
-          return readPoller(pin, cb);
-        }, CAPACITOR_POLL_DELAY);
+        //if (counter%2===0) {
+          setTimeout(function () {
+            readPoller(pin, cb);
+          }, 10);
+        //} else {
+        //  readPoller(pin, cb);
+        //}
       });
     };
 
     // discharge
+    // console.log('timing', pin);
     gpio.setDirection(pin, 'out', function (err) {
-      //logger.log('debug', '5');          
-      if (err) { isTiming[pin+''] = false;  logger.log('debug', 'error'); }
-      //logger.log('debug', '6');
+      if (err) { isTiming[pin+''] = false; logger.log('debug', 'error 2'); cb(-1); return; }
+      // console.log('timing 2', pin);
       gpio.write(pin, 0, function (err) {
-        //logger.log('debug', '7');
-        if (err) { isTiming[pin+''] = false;  logger.log('debug', 'error'); }
-        //logger.log('debug', '8');
-        setTimeout(function () {
-          //logger.log('debug', '9');
-          gpio.setDirection(pin, 'in', function (err) {
-            //logger.log('debug', '10');
-            if (err) { isTiming[pin+''] = false;  logger.log('debug', 'error'); }
-            //logger.log('debug', '11');
-            readPoller(pin, cb);
-          });
-        }, 50);
+        if (err) { isTiming[pin+''] = false; logger.log('debug', 'error 3'); cb(-1); return; }
+        // console.log('timing 3', pin);
+        gpio.setDirection(pin, 'in', function (err) {
+          if (err) { isTiming[pin+''] = false; logger.log('debug', 'error 4'); cb(-1); return; }
+          // console.log('timing 4', pin);
+          readPoller(pin, cb);
+        });
       });
     });
   };
 }());
 
 var colorTimer = function (pin, cb) {
-  logger.log('debug', '12');
   gpio.open(pin, 'out', function(err) {
-    //logger.log('debug', '13');
-    if (err) { logger.log('debug', 'error'); }
-    //logger.log('debug', '14');
+    if (err) { logger.log('debug', 'error 5'); /* console.log('close 5', pin, err);*/ gpio.close(pin, function (errClose) { /* console.log('closed 5', pin, errClose); */ cb(-1); }); return; }
     gpio.write(pin, 1, function(err) {
-      //logger.log('debug', '15');
-      if (err) { logger.log('debug', 'error'); }
-      //logger.log('debug', '16');
+      if (err) { logger.log('debug', 'error 6'); /*console.log('close 6', pin, err);*/ gpio.close(pin, function (errClose) { /* console.log('closed 6', pin, errClose); */ cb(-1); }); return; }
       pinTimer(photoPin, function (count) {
-        //logger.log('debug', '17');
+        // console.log('close', pin);
         gpio.close(pin, function () {
-          //logger.log('debug', '18');
+          // console.log('closed', pin); 
           cb(count);
         });
       });
@@ -122,22 +110,15 @@ var colorTimer = function (pin, cb) {
 };
 
 var isRGB = (function () {
-  /*
-   
-   */
-
   // lock variable so no more than one client can call this function
   var isRGBTiming = false;
   return function (cb) {
-    //logger.log('debug', '19');
     if (isRGBTiming) return;
     isRGBTiming = true;
     colorTimer(redPin, function (redCount) {
       //logger.log('debug', '20');
       colorTimer(greenPin, function (greenCount) {
-        //logger.log('debug', '21');
         colorTimer(bluePin, function (blueCount) {
-          //logger.log('debug', '22');
           isRGBTiming = false;
           cb({r: redCount, g: greenCount, b: blueCount});
         });
@@ -154,7 +135,14 @@ var btw = function (val, a, b) {
  */
 var checkColors = function (cb) {
   isRGB(function (val) {
-
+    var nextIteration = 0;
+    if (val.r === -1 || val.g === -1 || val.b === -1) {
+      nextIteration = 500;
+    }
+    if (val.r < 15 && val.g < 15 && val.b < 15) {
+      nextIteration = 500;
+    }
+    
     var output = {
       rgb: val,
       rgbString: '(' + val.r + ' ' + val.g  + ' ' + val.b + ')',
@@ -165,17 +153,7 @@ var checkColors = function (cb) {
       //isYellow: btw(val.r, 16, 20) && btw(val.g, 32, 41) && btw(val.b, 12, 13),
       //isYellow2: btw(val.r, 5, 15) && btw(val.g, 17, 20) && btw(val.b, 5, 14),
       isPurple: btw(val.r, 28, 34) && btw(val.g, 46, 50) && btw(val.b, 10, 11),
-      isGreen: btw(val.r, 26, 28) && btw(val.g, 32, 37) && btw(val.b, 8, 12),
-      //isGreen2: btw(val.r, 29, 31) && btw(val.g, 34, 39) && btw(val.b, 18, 20)
-      // isNone: btw(val.r, 6, 10) && btw(val.b, 6, 10),
-      // isRed: btw(val.r, 18, 26) && btw(val.g, 24, 39) && btw(val.b, 20, 26),
-      // isOrange: btw(val.r, 12, 17) && btw(val.g, 19, 25) && btw(val.b, 15, 18),
-      // isYellow: btw(val.r, 10, 16) && btw(val.g, 17, 20) && btw(val.b, 11, 16),
-      // //isYellow2: btw(val.r, 5, 15) && btw(val.g, 17, 20) && btw(val.b, 5, 14),
-      // isPurple: btw(val.r, 32, 42) && btw(val.g, 35, 49) && btw(val.b, 17, 22),
-      // isGreen: btw(val.r, 19, 21) && btw(val.g, 21, 23) && btw(val.b, 12, 15),
-      // isGreen2: btw(val.r, 29, 31) && btw(val.g, 34, 39) && btw(val.b, 18, 20)
-
+      isGreen: btw(val.r, 26, 28) && btw(val.g, 32, 37) && btw(val.b, 8, 12)
     };
     if (output.isRed) { output.colors.red = 1; }
     if (output.isOrange) { output.colors.orange = 1; }
@@ -185,8 +163,12 @@ var checkColors = function (cb) {
     if (output.isGreen) { output.colors.green = 1; }
     if (output.isGreen2) { output.colors.green2 = 1; }
 
-    cb(output);
-    checkColors(cb);
+    if (val.r !== -1 && val.g !== -1 && val.b !== -1 ) {
+      cb(output);
+    }
+    setTimeout(function () {
+      checkColors(cb);
+    }, nextIteration);
   });
 };
 
@@ -263,22 +245,55 @@ var playSong = (function () {
 // TODO
 // spawn('amixer', ['set', 'PCM', '--', '-0']);
 
+var summaryWindowSize = 10;
+var rs = [];
+var rSummary;
+var rLow;
+var rHigh;
+var gs = [];
+var gSummary;
+var gLow;
+var gHigh;
+var bs = [];
+var bSummary;
+var bLow;
+var bHigh;
 checkColors(function (val) {
   var colors = Object.keys(val.colors);
   var detected = colors.join(', ');
+
+  rs.unshift(val.rgb.r);
+  rs = rs.slice(0, summaryWindowSize);
+  rSummary = summary(rs.concat());
+  rLow = Math.round(rSummary.quartile(0.1));
+  rHigh = Math.round(rSummary.quartile(0.9));
+  gs.unshift(val.rgb.g);
+  gs = gs.slice(0, summaryWindowSize);
+  gSummary = summary(gs.concat());
+  gLow = Math.round(gSummary.quartile(0.1));
+  gHigh = Math.round(gSummary.quartile(0.9));
+  bs.unshift(val.rgb.b);
+  bs = bs.slice(0, summaryWindowSize);
+  bSummary = summary(bs.concat());
+  bLow = Math.round(bSummary.quartile(0.1));
+  bHigh = Math.round(bSummary.quartile(0.9));
+
+  var ranges = '(' + rLow + '-' + rHigh + ' ' + gLow + '-' + gHigh + ' ' + bLow + '-' + bHigh + ')';
+  var rangeSizes = '(' + (rHigh - rLow) + ', ' + (gHigh - gLow) + ', ' + (bHigh - bLow) + ')'; 
+
   if (program.debug) {
     if (val.isNone) {
       logger.log('info', '.');
     } else if (colors.length) {
-      logger.log('info', ' ' + val.rgbString + ') ' + detected); // process.stdout.write
+      logger.log('info', ' ' + val.rgbString + ') ' + ranges + ' ' + detected); // process.stdout.write
     } else {
-      logger.log('info', ' ' + val.rgbString + ')');
+      logger.log('info', ' ' + val.rgbString + ')' + ranges + ' ');
     }
   }
 
   if (program.calibrate) {
-    logger.log('info', val.rgbString + ' ' + detected);
-    console.log(val.rgbString + ' ' + detected);
+    logger.log('info', val.rgbString + ' ' + ranges + ' ' + rangeSizes + ' ' + detected);
+    console.log(val.rgbString + ' ' + ranges + ' ' + rangeSizes + ' ' + detected);
   }
 
   // detect streaks
